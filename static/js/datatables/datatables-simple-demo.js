f9d79cce121a9fdc7d0ab46695ec540e871dc6e5
Edenilson-Molina <Edenilsonmolina2001@hotmail.com>
2023-05-16 18:53:46 -0600

    Espacio de trabajo para el sistema

---
+++ static/js/datatables/datatables-simple-demo.js
@@ -0,0 +1,42 @@
+let dataTable;
+let dataTableIsInitialized = false;
+
+const dataTableOptions = {
+    //scrollX: "2000px",
+    lengthMenu: [5, 10, 15, 20, 100],
+    columnDefs: [
+        { searchable: false, targets: [7] },
+    ],
+    pageLength: 5,
+    destroy: true,
+    language: {
+        lengthMenu: "Mostrar _MENU_ registros por página",
+        zeroRecords: "Ningún usuario encontrado",
+        info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        infoEmpty: "Ningún usuario encontrado",
+        infoFiltered: "(filtrados desde _MAX_ registros totales)",
+        search: "Buscar:",
+        loadingRecords: "Cargando...",
+        paginate: {
+            first: "Primero",
+            last: "Último",
+            next: "Siguiente",
+            previous: "Anterior"
+        }
+    }
+};
+
+const initDataTable = async () => {
+    if (dataTableIsInitialized) {
+        dataTable.destroy();
+    }
+
+    dataTable = $("#datatablesSimple").DataTable(dataTableOptions);
+
+    dataTableIsInitialized = true;
+};
+
+
+window.addEventListener("load", async () => {
+    await initDataTable();
+});
